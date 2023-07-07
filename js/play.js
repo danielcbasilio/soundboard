5b645bcf38d0f1b93be942faee2ca24707baa714
danielbasilio <dbasilio15@gmail.com>
2023-07-07 14:54:29 +0100

    corrected minor bugs

--- js/play.js
+++ js/play.js
@@ -11,33 +11,32 @@ $(document).ready(function () {
 			})
 			.always(function (data) {
 //				data = data.responseText;
-				if (!data.localeCompare("die, you jerk")) {
+				if (data === "die, you jerk") {
 					swal("The specified file no longer exists!", "Please refresh the page!", "error", {
 						buttons: false,
 						timer: 5000,
 					});
-				} else if (!data.localeCompare("badIp")) {
+				} else if (data === "badIp") {
 					swal("Access denied!", "This button is only available from certain networks, and you are not in one of them!", "error", {
 						buttons: false,
 						timer: 5000,
 					});
-				} else if (!data.localeCompare("too long, you fool")) {
+				} else if (data === "too long, you fool") {
 					swal("Too long!", "The selected file is too long and cannot be played!", "error", {
 						buttons: false,
 						timer: 5000,
 					});
-				} else if (!data.localeCompare("forbidden")) {
+				} else if (data === "forbidden") {
 					swal("Estou farto desta merda!", "Deixem de ser chatos.", "error", {
 						buttons: false,
 						timer: 5000,
 					});
-				} else if (!data.localeCompare("success")) {
+				} else if (data === "success") {
 					swal("Sound Played!", "Give it another try!", "success", {
 						buttons: false,
 						timer: 1500,
 					});
 				}
-
 			});
 	});
 });
